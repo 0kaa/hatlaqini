319fef9ad9a4893219960f0348b0994541ea9bf2
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 21:55:10 +0200

    .

--- index.js
+++ index.js
@@ -13,23 +13,13 @@ const require = createRequire(import.meta.url);
 
 const app = express();
 var http = require("http").Server(app);
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '* or secific')
-  next();
-})
 
-var io = require("socket.io")(http, {
-  cors: {
-    origin: "https://hatlaqini.vercel.app/",
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-});
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.set('socketio', io);
+io.set('origins', '*:*');
 app.use("/", UserRoutes);
 app.use('/type', TypeRoutes);
 app.use("/locations", LocationRoutes);
@@ -40,7 +30,18 @@ app.use("/uploads", express.static("uploads"));
 
 const CONNECTION_URL = "mongodb+srv://mahmoud:8u4xwga99ahmiz1q@cluster0.bktlm.mongodb.net/hatlaqini?retryWrites=true&w=majority";
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '* or secific')
+  next();
+})
 
+var io = require("socket.io")(http, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 mongose
