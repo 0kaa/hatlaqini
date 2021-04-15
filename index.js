ca843d51dbe83ef5e6f473d21f85e697a15497d1
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 22:32:46 +0200

    .

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-var http = require("http").Server(app);
+var http = require("http").createServer();
 var io = require("socket.io")(http, {
   cors: {
     origin: '*',
