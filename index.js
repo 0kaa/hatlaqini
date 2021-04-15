ce97684fe0a7c68c6d08407cfd273987273827c3
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 22:23:12 +0200

    .

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import ItemsRoutes from "./routes/items.js";
 import ChatRoutes from "./routes/chat.js";
 import LocationRoutes from "./routes/location.js";
 import TypeRoutes from './routes/type.js';
+
+import { Server } from 'socket.io';
+
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
@@ -15,7 +18,7 @@ const app = express();
 var http = require("http").Server(app);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
+app.use(cors({ origin: '*' }));
 
 app.use("/", UserRoutes);
 app.use('/type', TypeRoutes);
@@ -26,20 +29,21 @@ app.use("/items", ItemsRoutes);
 app.use("/uploads", express.static("uploads"));
 const CONNECTION_URL = "mongodb+srv://mahmoud:8u4xwga99ahmiz1q@cluster0.bktlm.mongodb.net/hatlaqini?retryWrites=true&w=majority";
 
-var io = require("socket.io")(http, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
-});
+const io = new Server(http, { cors: { origin: '*' } });
+// var io = require("socket.io")(http, {
+//   handlePreflightRequest: (req, res) => {
+//     const headers = {
+//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//       "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+//       "Access-Control-Allow-Credentials": true
+//     };
+//     res.writeHead(200, headers);
+//     res.end();
+//   }
+// });
 app.set('socketio', io);
 
-app.use(cors({ origin: '*' }));
+
 
 
 const PORT = process.env.PORT || 5000;
