32424c57ec608f1faf7976c89648d0c93212f127
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 22:11:40 +0200

    1

--- index.js
+++ index.js
@@ -15,9 +15,27 @@ const app = express();
 var http = require("http").Server(app);
 
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 var io = require("socket.io")(http, {
   cors: {
