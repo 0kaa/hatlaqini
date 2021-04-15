ef0f707399be334f48a93d0f70aad7d786a78f21
mahmoud sabry <thebestoka0@gmail.com>
2021-04-15 21:58:32 +0200

    1

--- index.js
+++ index.js
@@ -18,17 +18,6 @@ var http = require("http").Server(app);
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.set('socketio', io);
-io.set('origins', '*:*');
-app.use("/", UserRoutes);
-app.use('/type', TypeRoutes);
-app.use("/locations", LocationRoutes);
-app.use("/chat", ChatRoutes);
-app.use("/categories", CategoriesRoutes);
-app.use("/items", ItemsRoutes);
-app.use("/uploads", express.static("uploads"));
-
-const CONNECTION_URL = "mongodb+srv://mahmoud:8u4xwga99ahmiz1q@cluster0.bktlm.mongodb.net/hatlaqini?retryWrites=true&w=majority";
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '* or secific')
@@ -42,6 +31,19 @@ var io = require("socket.io")(http, {
     credentials: true
   }
 });
+app.use("/", UserRoutes);
+app.use('/type', TypeRoutes);
+app.use("/locations", LocationRoutes);
+app.use("/chat", ChatRoutes);
+app.use("/categories", CategoriesRoutes);
+app.use("/items", ItemsRoutes);
+app.use("/uploads", express.static("uploads"));
+
+const CONNECTION_URL = "mongodb+srv://mahmoud:8u4xwga99ahmiz1q@cluster0.bktlm.mongodb.net/hatlaqini?retryWrites=true&w=majority";
+
+
+app.set('socketio', io);
+io.set('origins', '*:*');
 
 const PORT = process.env.PORT || 5000;
 mongose
